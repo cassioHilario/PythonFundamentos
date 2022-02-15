31cfe018f0b1db0d057e70afa2c7333c8197175f
Cássio Antônio Ferreira HIlário <cacaafh2000@gmail.com>
2022-02-14 23:09:09 -0300

    algumas aulas

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,45 @@
 
 print("\n******************* Python Calculator *******************")
 
+def primeiroNumero():
+    return int(input("Digite o primeiro número: "))
 
+def segundoNumero():
+    return int(input("Digite o segundo número: "))
+
+def opcoes(opcao):
+    print("\n\n")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - SAIR")
+    opcao2 = int(input("\n\n Escolha a operação: \n"))
+
+    if opcao2 == 1:
+        p = primeiroNumero()
+        s = segundoNumero()
+        print(p + s)
+        opcoes(opcao)
+    elif opcao2 == 2:
+        p = primeiroNumero()
+        s = segundoNumero()
+        print(p - s)
+        opcoes(opcao)
+    elif opcao2 == 3:
+        p = primeiroNumero()
+        s = segundoNumero()
+        print(p * s)
+        opcoes(opcao)
+    elif opcao2 == 4:
+        p = primeiroNumero()
+        s = segundoNumero()
+        print(p / s)
+        opcoes(opcao)
+    else:
+        print("saindo...")
+        exit()
+
+opcao = 0
+
+opcoes(opcao)
